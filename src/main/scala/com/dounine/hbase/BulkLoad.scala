a8096005519921b134f94f2eb6df6f83c58d637a
lake <102535481@qq.com>
2018-12-27 02:21:47 +0800

    hbase data bulkLoad

---
+++ src/main/scala/com/dounine/hbase/BulkLoad.scala
@@ -0,0 +1,82 @@
+package com.dounine.hbase
+
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.{HFileOutputFormat2, TableMapReduceUtil}
+import org.apache.hadoop.hbase.tool.LoadIncrementalHFiles
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration, KeyValue, TableName}
+import org.apache.hadoop.mapreduce.Job
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object BulkLoad {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf()
+      //      .setMaster("local[12]")
+      .setAppName("PhoenixBulkLoad")
+
+    val spark = SparkSession
+      .builder
+      .config(sparkConf)
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val datas = List(
+      ("abc", ("ext", "type", "login")),
+      ("ccc", ("ext", "type", "logout"))
+    )
+    val dataRdd = sc.parallelize(datas)
+
+    val output = dataRdd.map {
+      x => {
+        val rowKey = Bytes.toBytes(x._1)
+        val immutableRowKey = new ImmutableBytesWritable(rowKey)
+
+        val colFam = x._2._1
+        val colName = x._2._2
+        val colValue = x._2._3
+
+        val kv = new KeyValue(
+          rowKey,
+          Bytes.toBytes(colFam),
+          Bytes.toBytes(colName),
+          Bytes.toBytes(colValue.toString)
+        )
+        (immutableRowKey, kv)
+      }
+    }
+
+
+    val hConf = HBaseConfiguration.create()
+    hConf.addResource("hbase-site.xml")
+    val hTableName = "test_log"
+    hConf.set("hbase.mapreduce.hfileoutputformat.table.name", hTableName)
+
+    val job = Job.getInstance(hConf, "HbaseBulkLoad")
+    job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
+    job.setMapOutputValueClass(classOf[KeyValue])
+    TableMapReduceUtil.initCredentials(job)
+    val tableName = TableName.valueOf(hTableName)
+    val conn = ConnectionFactory.createConnection(hConf)
+    val table = conn.getTable(tableName)
+    val tableDescr = table.getDescriptor
+    val regionLocator = conn.getRegionLocator(tableName)
+    HFileOutputFormat2.configureIncrementalLoad(job, tableDescr, regionLocator)
+
+    val hFileOutput = "/tmp/h_file"
+
+    output.saveAsNewAPIHadoopFile(hFileOutput,
+      classOf[ImmutableBytesWritable],
+      classOf[KeyValue],
+      classOf[HFileOutputFormat2],
+      hConf
+    )
+
+    val bulkLoader = new LoadIncrementalHFiles(hConf)
+    bulkLoader.doBulkLoad(new Path(hFileOutput), conn.getAdmin, table, regionLocator)
+  }
+
+}
