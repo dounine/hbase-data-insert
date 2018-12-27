eaccd14014c6fa2b8c86d48573e4d2435268958f
lake <102535481@qq.com>
2018-12-27 16:39:16 +0800

    添加hbase普通写入

---
+++ src/main/scala/com/dounine/hbase/HbaseWrite.scala
@@ -0,0 +1,55 @@
+package com.dounine.hbase
+
+import org.apache.hadoop.hbase.{HBaseConfiguration, KeyValue, TableName}
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Put}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object HbaseWrite {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf()
+      //      .setMaster("local[12]")
+      .setAppName("HbaseWrite")
+
+    val spark = SparkSession
+      .builder
+      .config(sparkConf)
+      .getOrCreate()
+    val sc = spark.sparkContext
+
+    val datas = List(
+      ("abc", ("ext", "type", "login")),
+      ("ccc", ("ext", "type", "logout"))
+    )
+    val dataRdd = sc.parallelize(datas)
+
+    val output = dataRdd.map {
+      x => {
+        val rowKey = Bytes.toBytes(x._1)
+        val colFam = x._2._1
+        val colName = x._2._2
+        val colValue = x._2._3
+
+        val put = new Put(rowKey)
+        put.addColumn(Bytes.toBytes(colFam),
+          Bytes.toBytes(colName),
+          Bytes.toBytes(colValue.toString))
+      }
+    }
+
+    output.foreachPartition(it => {
+      val hConf = HBaseConfiguration.create()
+      hConf.addResource("hbase-site.xml")
+      val hTableName = "test_log"
+      val tableName = TableName.valueOf(hTableName)
+      val conn = ConnectionFactory.createConnection(hConf)
+      val table = conn.getTable(tableName)
+      while (it.hasNext) {
+        table.put(it.next())
+      }
+    })
+  }
+}
